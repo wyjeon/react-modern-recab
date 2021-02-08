200962a9548e55a8576c70596325bb9bb5adf8d2
wyjeon <wyjeon.info@gmail.com>
2021-02-09 05:17:28 +0900

    파일 정리

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 //실제 DOM내부에 리액트 컴포넌트를 랜더링한다.
 ReactDOM.render(
@@ -11,8 +9,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root') //id가 root인 DOM을 선택한다.
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
