145d5424418c1330eccbab44b6b8f6978391b19f
wyjeon <wyjeon.info@gmail.com>
2021-02-09 03:19:49 +0900

    3. 나의 첫번째 리액트 컴포넌트

---
+++ src/Hello.js
@@ -0,0 +1,8 @@
+import React from 'react'; //리액트를 불러온다.
+
+//함수 형태의 컴포넌트 (클래스 형태로도 작성이 가능하다.)
+function Hello() {
+  return <div>안녕</div>; //JSX
+}
+
+export default Hello; //컴포넌트를 내보낸다.
--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//실제 DOM내부에 리액트 컴포넌트를 랜더링한다.
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root') //id가 root인 DOM을 선택한다.
 );
 
 // If you want to start measuring performance in your app, pass a function
