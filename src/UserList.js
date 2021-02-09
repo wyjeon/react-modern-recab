a403660af48ce878e79b0836b15a98f038c05715
wyjeon <wyjeon.info@gmail.com>
2021-02-09 19:03:51 +0900

    16. useEffect를 사용하여 마운트/언마운트/업데이트시 할 작업 설정하기

--- src/UserList.js
+++ src/UserList.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-// onRemove()는 id가 __인 객체를 삭제한다.
 function User({ user, onRemove, onToggle }) {
+  /*
+  useEffect(() => {}, [])
+  첫 번째 파라미터에는 함수,
+  두 번째 파라미터에는 의존값이 들어있는 배열(deps)
+
+  deps를 비우면 
+  컴포넌트가 처음 나타날때만 
+  useEffect에 등록한 함수가 호출된다.
+
+  deps에 특정 값을 넣으면
+  컴포넌트가 처음 마운트 될 때도 호출되고
+  지정한 값이 바뀔 때도 호출되고
+  언마운트시에도 호출되고
+  값이 바뀌기 직전에도 호출된다.
+
+  deps 파라미터를 생략한다면 -> useEffect(() => {})
+  컴포넌트가 리렌더링 될 때마다 호출된다.
+  */
+
+  useEffect(() => {
+    console.log('user 값이 설정됨');
+    console.log(user);
+    return () => {
+      console.log('user 가 바뀌기 전..');
+      console.log(user);
+    };
+  }, [user]);
   return (
     <div>
-      {/* 
-        active의 값에 따라 폰트 색상을 바꾼다.
-        마우스를 올렸을 때 커서를 바꾼다.
-      */}
       <b
         style={{
           cursor: 'pointer',
@@ -17,7 +39,7 @@ function User({ user, onRemove, onToggle }) {
       >
         {user.username}
       </b>
-
+      &nbsp;
       <span>({user.email})</span>
       <button onClick={() => onRemove(user.id)}>삭제</button>
     </div>
